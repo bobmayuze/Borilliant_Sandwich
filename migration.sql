c9f17dbcdbfd1dbfa7ba71fc489a6a222b1ed7c0
Yuze.Ma <584653629@qq.com>
2017-11-01 16:00:42 -0400

    Update reademe with usage of google drive CDN, and new migration file

--- migration.sql
+++ migration.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 01, 2017 at 08:21 PM
+-- Generation Time: Nov 01, 2017 at 08:57 PM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -23,6 +23,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_combos`
+--
+
+CREATE TABLE `tbl_combos` (
+  `id` int(11) NOT NULL,
+  `creater_id` int(11) NOT NULL DEFAULT '999999',
+  `bread_id` int(11) NOT NULL,
+  `bread_qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_ingredient_bread`
 --
 
@@ -33,12 +46,11 @@ CREATE TABLE `tbl_ingredient_bread` (
   `pictureURL` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `tbl_ingredient_meat` (
-  `id` int(11) NOT NULL,
-  `name` varchar(80) NOT NULL,
-  `calories` int(11) NOT NULL,
-  `pictureURL` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ingredient_cheese`
+--
 
 CREATE TABLE `tbl_ingredient_cheese` (
   `id` int(11) NOT NULL,
@@ -47,12 +59,24 @@ CREATE TABLE `tbl_ingredient_cheese` (
   `pictureURL` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `tbl_ingredient_vegetable` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ingredient_meat`
+--
+
+CREATE TABLE `tbl_ingredient_meat` (
   `id` int(11) NOT NULL,
   `name` varchar(80) NOT NULL,
   `calories` int(11) NOT NULL,
   `pictureURL` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ingredient_sauce`
+--
 
 CREATE TABLE `tbl_ingredient_sauce` (
   `id` int(11) NOT NULL,
@@ -61,42 +85,138 @@ CREATE TABLE `tbl_ingredient_sauce` (
   `pictureURL` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ingredient_vegetable`
+--
+
+CREATE TABLE `tbl_ingredient_vegetable` (
+  `id` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `calories` int(11) NOT NULL,
+  `pictureURL` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(512) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `nickname` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `nickname`) VALUES
+(1, 'admin', '123456', NULL),
+(2, 'admin2', '123456', NULL);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_combos`
+--
+ALTER TABLE `tbl_combos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `creater_id` (`creater_id`),
+  ADD KEY `bread_id` (`bread_id`);
 
 --
 -- Indexes for table `tbl_ingredient_bread`
 --
 ALTER TABLE `tbl_ingredient_bread`
   ADD PRIMARY KEY (`id`);
-ALTER TABLE `tbl_ingredient_meat`
-  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_ingredient_cheese`
+--
 ALTER TABLE `tbl_ingredient_cheese`
   ADD PRIMARY KEY (`id`);
-ALTER TABLE `tbl_ingredient_vegetable`
+
+--
+-- Indexes for table `tbl_ingredient_meat`
+--
+ALTER TABLE `tbl_ingredient_meat`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_ingredient_sauce`
+--
 ALTER TABLE `tbl_ingredient_sauce`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `tbl_ingredient_vegetable`
+--
+ALTER TABLE `tbl_ingredient_vegetable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_combos`
+--
+ALTER TABLE `tbl_combos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `tbl_ingredient_bread`
 --
 ALTER TABLE `tbl_ingredient_bread`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `tbl_ingredient_meat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_ingredient_cheese`
+--
 ALTER TABLE `tbl_ingredient_cheese`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `tbl_ingredient_vegetable`
+--
+-- AUTO_INCREMENT for table `tbl_ingredient_meat`
+--
+ALTER TABLE `tbl_ingredient_meat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_ingredient_sauce`
+--
 ALTER TABLE `tbl_ingredient_sauce`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_ingredient_vegetable`
+--
+ALTER TABLE `tbl_ingredient_vegetable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_combos`
+--
+ALTER TABLE `tbl_combos`
+  ADD CONSTRAINT `tbl_combos_ibfk_1` FOREIGN KEY (`creater_id`) REFERENCES `tbl_user` (`id`),
+  ADD CONSTRAINT `tbl_combos_ibfk_2` FOREIGN KEY (`bread_id`) REFERENCES `tbl_ingredient_bread` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

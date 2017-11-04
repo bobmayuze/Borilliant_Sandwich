e1c46375fcda3dde45412b865e049901defc4b29
carlsc3 <h.warrior14@gmail.com>
2017-11-03 20:43:11 -0400

    Edit cheese table, 4 cheeses

--- tbl_ingredient_cheese.sql
+++ tbl_ingredient_cheese.sql
@@ -5,6 +5,4 @@ INSERT INTO `tbl_ingredient_cheese` (`id`, `name`, `calories`, `pictureURL`) VAL
 (1, 'swiss', 106, 'https://boygeniusreport.files.wordpress.com/2015/05/swiss_cheese.jpg?quality=98&strip=all'),
 (2, 'provolone', 96, 'http://www.murrayscheese.com/site/images/items/20019700000.0.jpg?resizeid=3&resizeh=600&resizew=600'),
 (3, 'american', 104, 'http://thumbs.ifood.tv/files/images/editor/images/American%20Cheese.jpg'),
-(4, 'pepper_jack', 100, 'https://www.walnutcreekfarmtexas.com/wp-content/uploads/2014/05/PEPPER-JACK_ALT_525x350.jpg'),
-(5, 'mozzarella', 78, 'https://goldenagecheese.com/wp-content/uploads/2014/03/smoke_mozz.jpg'),
-(6, 'monterey_jack', 64, 'https://www.freshdirect.com/media/images/product/deli_2/dcjk_mntjk_z.jpg?lastModify=2009-05-06');
+(4, 'cheddar', 113, 'http://www.holypine.com/wp-content/uploads/2015/10/Cheddar-Cheese-600x600.jpg');

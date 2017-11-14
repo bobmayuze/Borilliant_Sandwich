5a47a331ccb638dd2f1c167aa700e7907629ca8b
Andrew <adleaf1@gmail.com>
2017-11-14 00:22:53 -0500

    Added read table API's for bread, cheese, meat, sauce, and vegetable tables

---
+++ htdocs/api/read_table_vegetable.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gredients";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$result = $conn->query("SELECT * FROM tbl_ingredient_vegetable");
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"name":"'  . $rs["name"] . '",';
+    $outp .= '"calories":"'   . $rs["calories"] . '",';
+    $outp .= '"pictureURL":"'   . $rs["pictureURL"] . '"}';
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+?>
